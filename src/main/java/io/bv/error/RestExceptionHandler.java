60b57d9d161fbc0249a6b04d2ebeafc229cf8973
sayid.sidqi <sayid.sidqi@gdn-commerce.com>
2021-11-19 01:08:27 +0700

    integration w boot: full i18n

--- src/main/java/io/bv/error/RestExceptionHandler.java
+++ src/main/java/io/bv/error/RestExceptionHandler.java
@@ -2,6 +2,8 @@ package io.bv.error;
 
 import com.blibli.oss.backend.common.model.response.Response;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.bind.support.WebExchangeBindException;
@@ -9,6 +11,7 @@ import reactor.core.publisher.Mono;
 
 import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 import java.util.stream.Collectors;
 
@@ -18,13 +21,18 @@ import static com.blibli.oss.backend.common.helper.ResponseHelper.badRequest;
 @RestControllerAdvice
 public class RestExceptionHandler {
 
+  @Autowired
+  private MessageSource messageSource;
+
   @ExceptionHandler(WebExchangeBindException.class)
-  public Mono<Response<?>> handleWebExchangeBindException(WebExchangeBindException ex) {
+  public Mono<Response<?>> handleWebExchangeBindException(WebExchangeBindException ex, Locale locale) {
     Map<String, List<String>> errors = new HashMap<>();
     errors.put("messages", ex
       .getFieldErrors()
       .stream()
-      .map(fieldError -> String.format("field %s invalid: %s", fieldError.getField(), fieldError.getDefaultMessage()))
+      .map(fieldError -> messageSource.getMessage(
+        "field.invalid", new Object[]{fieldError.getField(), fieldError.getDefaultMessage()}, "field.invalid", locale
+      ))
       .collect(Collectors.toList())
     );
     return Mono.just(badRequest(errors));
