97b44d7fadb9b91c23168db574493dfb4a20f9d6
sayid.sidqi <sayid.sidqi@gdn-commerce.com>
2021-11-19 01:30:17 +0700

    @validated?: validation group

--- src/main/java/io/bv/controller/ProductController.java
+++ src/main/java/io/bv/controller/ProductController.java
@@ -2,9 +2,12 @@ package io.bv.controller;
 
 import com.blibli.oss.backend.common.model.response.Response;
 import io.bv.model.Product;
+import io.bv.validation.group.Create;
+import io.bv.validation.group.Update;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -13,6 +16,7 @@ import reactor.core.publisher.Mono;
 
 import static com.blibli.oss.backend.common.helper.ResponseHelper.status;
 import static org.springframework.http.HttpStatus.CREATED;
+import static org.springframework.http.HttpStatus.NO_CONTENT;
 
 @Slf4j
 @RestController
@@ -21,9 +25,16 @@ public class ProductController {
 
   @PostMapping
   @ResponseStatus(CREATED)
-  public Mono<Response<Void>> create(@RequestBody @Validated Product product) {
+  public Mono<Response<Void>> create(@RequestBody @Validated(Create.class) Product product) {
     log.info("Product {} created", product.getName());
     return Mono.just(status(CREATED));
+  }
+
+  @PutMapping
+  @ResponseStatus(NO_CONTENT)
+  public Mono<Response<Void>> update(@RequestBody @Validated(Update.class) Product product) {
+    log.info("Product {} updated", product.getName());
+    return Mono.just(status(NO_CONTENT));
   }
 
 }
