a6511898049e3859c9ec428cb5eb65574f27f1b7
sayid.sidqi <sayid.sidqi@gdn-commerce.com>
2021-11-19 01:15:15 +0700

    @validated?

--- src/main/java/io/bv/controller/ProductController.java
+++ src/main/java/io/bv/controller/ProductController.java
@@ -3,14 +3,13 @@ package io.bv.controller;
 import com.blibli.oss.backend.common.model.response.Response;
 import io.bv.model.Product;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
-
-import javax.validation.Valid;
 
 import static com.blibli.oss.backend.common.helper.ResponseHelper.status;
 import static org.springframework.http.HttpStatus.CREATED;
@@ -22,7 +21,7 @@ public class ProductController {
 
   @PostMapping
   @ResponseStatus(CREATED)
-  public Mono<Response<Void>> create(@RequestBody @Valid Product product) {
+  public Mono<Response<Void>> create(@RequestBody @Validated Product product) {
     log.info("Product {} created", product.getName());
     return Mono.just(status(CREATED));
   }
